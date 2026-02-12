0e55707969a406a37cd8fb463dcf419b15a92a97
Baraa <156128855+Baraa-op@users.noreply.github.com>
2026-02-12 22:16:27 +0300

    Create 2.cpp

---
+++ 2.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include<iomanip>
+using namespace std;
+
+
+
+
+int Random_Number(int From, int To)
+{
+    int Random = rand() % (To - From + 1) + From ;
+    return Random ;
+}
+
+void Fill_Matrix_With_Random_Number(int arr[3][3])
+{
+    for(int i = 0 ; i <= 2; i++)
+    {
+        for(int j = 0 ; j <= 2; j++)
+            {
+                arr[i][j] = Random_Number(1 , 100) ;
+            }
+    }
+}
+
+
+void Sum_Of_Row_Matrix(int arr[3][3])
+{
+    
+    
+    cout <<"\n\nSum of matrix rows : \n" ;
+    
+    for(int i = 0 ; i <= 2; i++)
+    {
+        
+        
+        int Sum = 0 ;
+        
+        for(int j = 0 ; j <= 2; j++)
+            {
+                Sum+=arr[i][j] ;
+                
+            }
+            
+            cout << "row " << i+1 << " Sum = " << Sum << "\n" ;
+    }
+}
+
+
+void Print_Matrix(int arr[3][3])
+{
+    for(int i = 0 ; i <= 2; i++)
+    {
+        for(int j = 0 ; j <= 2; j++)
+            {
+                cout << left << setw(3) << arr[i][j] << "   "  ;
+            }
+            cout << "\n" ;
+    }
+}
+
+int main() 
+{
+    srand((unsigned)time(NULL)) ;
+    
+    int arr[3][3] ;
+    Fill_Matrix_With_Random_Number(arr) ;
+    Print_Matrix(arr) ;
+    Sum_Of_Row_Matrix(arr);
+}
+    
